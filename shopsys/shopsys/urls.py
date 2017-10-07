6f21b30ad939f75dfe700249f5cac7e12b8ac35e
MasterShu <1562688988@qq.com>
2017-10-07 18:51:09 +0800

    init

---
+++ shopsys/shopsys/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for shopsys.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', shopsys.views.home, name='home'),
+    # url(r'^shopsys/', include('shopsys.shopsys.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
